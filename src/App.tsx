3a5ff6102d732be761eb9b71b8335507af799202
Taha-Jamal <tjamal618@gmail.com>
2024-02-10 20:30:15 +0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import Expense_tracker from "./Components/Expense_tracker"
-
+import { useState } from "react";
+import ExpenseList from "./Components/ExpenseList";
+import Expense_tracker from "./Components/Expense_tracker";
 
 function App() {
- 
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "rent", amount: 35, catagory: "ulti" },
+    { id: 2, description: "renta", amount: 35, catagory: "ulti" },
+    { id: 3, description: "rentb", amount: 35, catagory: "ulti" },
+  ]);
 
   return (
     <div>
       <Expense_tracker />
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/ExpenseList.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+interface Expense {
+  id: number;
+  description: string;
+  amount: number;
+  catagory: string;
+}
+
+interface Props {
+  expenses: Expense[];
+  onDelete: (id: number) => void;
+}
+const ExpenseList = ({ expenses, onDelete }: Props) => {
+    if(expenses.length === 0) return null
+
+  return (
+    <table className="border-2 border-collapse">
+      <thead className="border-2 h-10">
+        <tr>
+          <th>Descripirion</th>
+          <th>Amount</th>
+          <th>Catagory</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody className="border-2 h-10">
+        {expenses.map((expense) => (
+          <tr key={expense.id}>
+            <td>{expense.description}</td>
+            <td>{expense.amount}</td>
+            <td>{expense.catagory}</td>
+            <td>
+              <button
+                onClick={() => onDelete(expense.id)}
+                className="border-2 border-red-600 rounded-xl text-red-600 w-20 hover:text-black hover:border-black hover:bg-red-600 "
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+      <tfoot className="border-2 h-10">
+        <tr>
+          <td>Total</td>
+          <td>
+            $
+            {expenses
+              .reduce((acc, expense) => expense.amount + acc, 0)
+              .toFixed(2)}
+          </td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tfoot>
+    </table>
+  );
+};
+
+export default ExpenseList;
